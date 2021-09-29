4155410bd87549266dc272aa84a1398eaf74b743
Kristian Aars <kristian@aars.me>
2021-09-29 22:40:05 +0200

    Implemented multiplicative hash method. Support for ordinary int-key and String.

--- src/no/ntnu/idi/krisvaa/idatt2101/Hash.java
+++ src/no/ntnu/idi/krisvaa/idatt2101/Hash.java
@@ -3,6 +3,19 @@ package no.ntnu.idi.krisvaa.idatt2101;
 public class Hash {
 
     /**
+     * @param s String to be hashed
+     * @param x Number of bits in tablesize (Where tablesize m = 2^x)
+     * @return Hashed key-value
+     */
+    public static int multhash(String s, int x) {
+        int k = 1;
+        for(int i = 1; i < s.length(); i++) {
+            k = s.charAt(i - 1) + 113 * k;
+        }
+        return multhash(k, x);
+    }
+
+    /**
      * @param k Key to be hashed
      * @param m NUmber of elements in hashtable
      * @return
@@ -11,14 +24,15 @@ public class Hash {
         return k % m;
     }
 
-
     /**
      * @param k Key to be hashed
      * @param x Number of bits in tablesize (Where tablesize m = 2^x)
      * @return Hashed key-value
      */
-    private static int A = 1327217885;
+    private static long A = 2654435769L;
     public static int multhash(int k, int x) {
-        return k * A >> (31 - x);
+        int kA = (int) (k * A);
+        return kA > 0 ? kA >> (31 - x) : (kA * -1) >> (31 - x);
     }
+
 }
