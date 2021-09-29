8b053a18a0d7cd627264191c1d3bc5216b7cb8eb
Kristian Aars <kristian@aars.me>
2021-09-29 22:41:02 +0200

    Now supports adding elements to table. Automatic hashing and collision detection.

--- src/no/ntnu/idi/krisvaa/idatt2101/HashTable.java
+++ src/no/ntnu/idi/krisvaa/idatt2101/HashTable.java
@@ -2,22 +2,79 @@ package no.ntnu.idi.krisvaa.idatt2101;
 
 import com.sun.jdi.InvalidTypeException;
 
-public class HashTable <T> {
+import java.util.Arrays;
+import java.util.LinkedList;
 
-    public HashTable() {
+public class HashTable {
 
+    private LinkedList<String>[] elements;
+    private int size;
+    private int x;
+    private int count = 0;
+
+    private int collisionCounter = 0;
+
+    public HashTable(int size) {
+        this.size = findNearestPowerOfTwo(size);
+        this.x = (int) (Math.log(this.size) / Math.log(2));
+        elements = new LinkedList[this.size];
+
+        for(int i = 0; i < this.size; i++) { elements[i] = new LinkedList<>(); };
     }
 
-    public int insert(T o) {
+    private int findNearestPowerOfTwo(int n) {
+        int power = 2;
+        while (power < n) {
+            power = power << 1;
+        }
 
+        return power;
+    }
+
+    public int insert(String s) {
+        int k = Hash.multhash(s, x);
+
+        boolean collision = !elements[k].isEmpty();
+        if (collision) collisionCounter++;
+
+        elements[k].addLast(s);
+        count++;
+
+        return k;
     }
 
     public int delete() {
         return -1;
     }
 
-    public Object find(int key) {
-        return null;
+    public Object find(int k) {
+        return elements[k];
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" +
+                "size=" + size +
+                ", x=" + x +
+                ", count=" + count +
+                ", collisions=" + collisionCounter +
+                ", elements=" + toFormattedTable(true) +
+                "\n}";
+    }
+
+    public String toFormattedTable(boolean showEmpty) {
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            LinkedList<String> l = elements[i];
+
+            if(showEmpty || !l.isEmpty()) {
+                s += "\n["+i+"]= ";
+                for(String e : l) {
+                    s += ""+e+"; ";
+                }
+            }
+        }
+        return s;
     }
 
 }
